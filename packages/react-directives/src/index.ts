66e117b13ada9819f84189c24ecb89befa6015c0
Alem Tuzlak <t.zlak@hotmail.com>
2025-05-20 15:40:38 +0200

    Add support for custom directives

--- packages/react-directives/src/index.ts
+++ packages/react-directives/src/index.ts
@@ -1,23 +1,23 @@
 import type { Plugin } from "vite"
 import { transformCode } from "./transformer"
 
-// This is your packages entry point, everything exported from here will be accessible to the end-user.
-export const reactDirectives = (props?: {
+export interface ReactDirectivesPluginOptions {
 	"use strict"?: string[]
 	"use server"?: string[]
 	"use client"?: string[]
+	[key: `use ${string}`]: string[] | undefined
+}
+
+// This is your packages entry point, everything exported from here will be accessible to the end-user.
+export const reactDirectives = (
+	props?: ReactDirectivesPluginOptions
 	// biome-ignore lint/suspicious/noExplicitAny: Avoid issues with vite-plugin types
-}): any => {
-	const { "use client": useClient = [], "use server": useServer = [], "use strict": useStrict = [] } = props || {}
+): any => {
 	return {
 		name: "react-directives-plugin",
 		enforce: "pre",
 		transform(code, id) {
-			return transformCode(code, id, {
-				useClient,
-				useServer,
-				useStrict,
-			})
+			return transformCode(code, id, props)
 		},
 	} satisfies Plugin
 }
